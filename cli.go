b49750d42165659f6deee4a7c14fa02fa608f422
kimjuls <iloveamalamute@gmail.com>
2024-08-22 08:41:30 +0900

    fixed cli.go

--- cli.go
+++ cli.go
@@ -11,8 +11,27 @@ type CLI struct {
 	bc *Blockchain
 }
 
+func (cli *CLI) printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("  createblockchain -address ADDRESS - Create a blockchain and send genesis block reward to ADDRESS")
+	fmt.Println("  createwallet - Generates a new key-pair and saves it into the wallet file")
+	fmt.Println("  getbalance -address ADDRESS - Get balance of ADDRESS")
+	fmt.Println("  listaddresses - Lists all addresses from the wallet file")
+	fmt.Println("  printchain - Print all the blocks of the blockchain")
+	fmt.Println("  reindexutxo - Rebuilds the UTXO set")
+	fmt.Println("  send -from FROM -to TO -amount AMOUNT -mine - Send AMOUNT of coins from FROM address to TO. Mine on the same node, when -mine is set.")
+	fmt.Println("  startnode -miner ADDRESS - Start a node with ID specified in NODE_ID env. var. -miner enables mining")
+}
+
+func (cli *CLI) validateArgs() {
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		os.Exit(1)
+	}
+}
+
 func (cli *CLI) Run() {
-	// cli.validateArgs()
+	cli.validateArgs()
 
 	addBlockCmd := flag.NewFlagSet("addblock", flag.ExitOnError)
 	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
@@ -30,7 +49,7 @@ func (cli *CLI) Run() {
 			panic(err)
 		}
 	default:
-		// cli.printUsage()
+		cli.printUsage()
 		os.Exit(1)
 	}
 
